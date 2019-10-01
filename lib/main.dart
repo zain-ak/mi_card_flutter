56a978e2d60e11d56d810175594ba627a6dede6a
Zain Khalid <zak565@uowmail.edu.au>
2019-10-01 02:16:30 -0400

    Layout Practice eh

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,48 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+    MiCard()
   );
+}
+
+class MiCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.purple[900],
+        body: SafeArea(
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget> [
+              Container(
+                width: 100,
+                color: Colors.yellow
+              ),
+              Column (
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    color: Colors.blue
+                  ),
+                  Container(
+                      height: 100.0,
+                      width: 100.0,
+                      color: Colors.orange
+                  ),
+                ],
+              ),
+              Container (
+                  width: 100,
+                  color: Colors.pink
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
