e3efcf665124a88042b75448408bd2b6ff1a2767
Zain Khalid <zak565@uowmail.edu.au>
2019-10-02 03:35:52 -0400

    Continued with module, almost complete with final stuff. Using an additional libraries for loading in SVGs. Yay.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
 
 void main() {
   runApp(
@@ -13,34 +15,63 @@ class MiCard extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.purple[900],
         body: SafeArea(
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget> [
-              Container(
-                width: 100,
-                color: Colors.yellow
-              ),
-              Column (
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    height: 100.0,
-                    width: 100.0,
-                    color: Colors.blue
-                  ),
-                  Container(
-                      height: 100.0,
-                      width: 100.0,
-                      color: Colors.orange
-                  ),
-                ],
-              ),
-              Container (
-                  width: 100,
-                  color: Colors.pink
-              ),
-            ],
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar (
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/professional_zain.jpeg')
+                ),
+                SizedBox (height: 15.0),
+                Text (
+                  'Zain Khalid',
+                    style: TextStyle(
+                      letterSpacing: 0.5,
+                      color: Colors.orange[500],
+                      fontSize: 40.0,
+                      fontFamily: 'AzoSans',
+                      fontWeight: FontWeight.w600
+                    ),
+                ),
+                SizedBox (height: 5.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SvgPicture.asset (
+                      'svg/flutter.svg',
+                      height: 30.0,
+                    ),
+                    SizedBox (width: 10.0),
+                    SvgPicture.asset (
+                      'svg/android.svg',
+                      height: 30.0,
+                    ),
+                    SizedBox (width: 10.0),
+                    SvgPicture.asset (
+                      'svg/js.svg',
+                      height: 30.0,
+                    ),
+                    SizedBox (width: 15.0),
+                    Text (
+                      'Developer',
+                      style: TextStyle(
+                        fontFamily: 'AzoSans',
+                        fontWeight: FontWeight.w300,
+                        color: Colors.orange[400],
+                        fontSize: 25.0
+                      ),
+                    ),
+                  ],
+                ),
+                Container (
+
+                  child: Row (
+
+                  )
+                )
+              ],
+            ),
           ),
         ),
       ),
