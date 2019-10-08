a0769e691a4da8fb2fbbeeeca96290bf112618a8
Zain Khalid <zak565@uowmail.edu.au>
2019-10-08 02:25:00 -0400

    - Removed copyrighted font and added an opensource one 🤗 - Added a github.svg - Added 3 methods; _launchGit(), _launchPhone() & _launchMail() using   the url_launcher library - Wrapped the Container 'cards' around an InkWell widget for onTap   functionality - svgAsset is just cool man.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(
     MiCard()
   );
+}
+
+_launchGit() async {
+  const url = 'https://github.com/zain-ak';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+_launchPhone() async {
+  const url = 'tel: +1 647 906 9242';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
+
+_launchMail() async {
+  const url = 'mailto:zain@zainkhalid.me?subject=Hey%20Zain!';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
 }
 
 class MiCard extends StatelessWidget {
@@ -30,7 +58,7 @@ class MiCard extends StatelessWidget {
                       letterSpacing: 0.5,
                       color: Colors.orange[500],
                       fontSize: 40.0,
-                      fontFamily: 'AzoSans',
+                      fontFamily: 'Mansalva',
                       fontWeight: FontWeight.w600
                     ),
                 ),
@@ -56,20 +84,102 @@ class MiCard extends StatelessWidget {
                     Text (
                       'Developer',
                       style: TextStyle(
-                        fontFamily: 'AzoSans',
-                        fontWeight: FontWeight.w300,
+                        fontFamily: 'Mansalva',
+                        fontWeight: FontWeight.w500,
                         color: Colors.orange[400],
                         fontSize: 25.0
                       ),
                     ),
                   ],
                 ),
-                Container (
-
-                  child: Row (
-
-                  )
-                )
+                InkWell(
+                  onTap: _launchGit,
+                  child: Container (
+                    decoration: BoxDecoration (
+                      gradient: LinearGradient(begin: Alignment(0.25, 0.0), end: Alignment(1.0, 0), colors: [Colors.white, Colors.purple[300]]),
+                      borderRadius: BorderRadius.all(Radius.circular(20.0))
+                    ),
+//                  color: Colors.white,
+                    margin: EdgeInsets.symmetric(vertical: 30.0, horizontal: 50.0),
+                    padding: EdgeInsets.all(10.0),
+                    child: Row (
+                      children: <Widget>[
+                        SvgPicture.asset(
+                        'svg/github.svg',
+                        height: 30.0,
+                        color: Colors.purple[500],
+                        ),
+                        SizedBox (width: 15.0),
+                        Text (
+                            'github.com/zain-ak',
+                          style: TextStyle(
+                            color: Colors.purple[900],
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.w500
+                          )
+                        ),
+                      ],
+                    )
+                  ),
+                ),
+                InkWell(
+                  onTap: _launchPhone,
+                  child: Container (
+                    decoration: BoxDecoration (
+                        gradient: LinearGradient(begin: Alignment(0.25, 0.0), end: Alignment(1.0, 0), colors: [Colors.white, Colors.purple[300]]),
+                        borderRadius: BorderRadius.all(Radius.circular(20.0))
+                    ),
+    //                  color: Colors.white,
+                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 50.0),
+                    padding: EdgeInsets.all(10.0),
+                    child: Row (
+                      children: <Widget>[
+                        Icon (
+                          Icons.phone_android,
+                          color: Colors.purple[500]
+                        ),
+                        SizedBox (width: 15.0),
+                        Text (
+                            '647.906.9242',
+                            style: TextStyle(
+                                color: Colors.purple[900],
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.w500
+                            )
+                        ),
+                      ],
+                    )
+                  ),
+                ),
+                InkWell(
+                  onTap: _launchMail,
+                  child: Container (
+                      decoration: BoxDecoration (
+                          gradient: LinearGradient(begin: Alignment(0.25, 0.0), end: Alignment(1.0, 0), colors: [Colors.white, Colors.purple[300]]),
+                          borderRadius: BorderRadius.all(Radius.circular(20.0))
+                      ),
+                      //                  color: Colors.white,
+                      margin: EdgeInsets.symmetric(vertical: 30.0, horizontal: 50.0),
+                      padding: EdgeInsets.all(10.0),
+                      child: Row (
+                        children: <Widget>[
+                          Icon (
+                              Icons.email,
+                              color: Colors.purple[500]
+                          ),
+                          SizedBox (width: 15.0),
+                          Text (
+                              'zain@zainkhalid.me',
+                              style: TextStyle(
+                                  color: Colors.purple[900],
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.w500
+                              )
+                          ),
+                        ],
+                      )
+                  ),
+                ),
               ],
             ),
           ),
@@ -77,4 +187,7 @@ class MiCard extends StatelessWidget {
       ),
     );
   }
+
 }
+
+
